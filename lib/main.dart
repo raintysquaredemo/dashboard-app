dc643f1e36ad6127fefeade67ce33a6bb3b774a5
raintysquaredemo <dosomethingnonestop@gmail.com>
2024-01-10 09:57:14 +0800

    Add new text line

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,8 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             Row(
               children: [
-                Text('Dashboard')
+                Text('Dashboard'),
+		Text('Hello world'),
               ],
             )
           ],
